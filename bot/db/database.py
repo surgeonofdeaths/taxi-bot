b5af017a4748ef5ee32e0b11abff28025f3e8814
Artem Holodyr <artemmholodyr@gmail.com>
2024-04-20 02:32:21 +0300

    remove lines that print current directory

--- bot/db/database.py
+++ bot/db/database.py
@@ -2,11 +2,6 @@
 # from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-import os
-
-current_working_directory = os.getcwd()
-print(current_working_directory)
-
 from config.config import settings
 from loguru import logger
 
