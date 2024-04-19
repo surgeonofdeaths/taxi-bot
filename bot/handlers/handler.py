7ccc6d11b2cb6634fff10ca04348e699d8652e41
Artem Holodyr <artemmholodyr@gmail.com>
2024-04-20 02:37:18 +0300

    /start: adds user to a database

--- bot/handlers/handler.py
+++ bot/handlers/handler.py
@@ -1,15 +1,22 @@
 from aiogram import Router
-from aiogram.types import Message
-
 from aiogram.filters import Command
+from aiogram.types import Message
 from loguru import logger
+from services import db_create_user
+from sqlalchemy.ext.asyncio import AsyncSession
 
 router = Router()
 
 
 @router.message(Command(commands=["start"]))
-async def process_start_command(message: Message):
+async def process_start_command(message: Message, session: AsyncSession):
+    await db_create_user(message.from_user, session)
     await message.answer("Добро пожаловать в бота для заказа такси!")
+
+
+@router.message(Command(commands=["order"]))
+async def process_order_command(message: Message):
+    await message.answer("order")
 
 
 @router.message()
