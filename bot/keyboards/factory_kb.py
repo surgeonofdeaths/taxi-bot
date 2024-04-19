10e6d0d41153afb7926471caf14b48cdf45988e0
Artem Holodyr <artemmholodyr@gmail.com>
2024-04-20 02:31:26 +0300

    add folder keyboards and empty files

---
+++ bot/keyboards/factory_kb.py
@@ -0,0 +1,8 @@
+# from aiogram.filters.callback_data import CallbackData
+
+
+# class OrderCallbackFactory(CallbackData, prefix="ord"):
+#     title: str | None
+#     uuid: str | None
+#     is_file: bool = True
+#     action: str  # dir, file
---
+++ bot/keyboards/keyboard.py
@@ -0,0 +1,19 @@
+# from aiogram.utils.keyboard import (
+#     InlineKeyboardBuilder,
+#     InlineKeyboardButton,
+#     InlineKeyboardMarkup,
+# )
+
+# from keyboards.factory_kb import NavigationCallbackFactory
+# from lexicon.lexicon import LEXICON
+# from services import megacmd, other
+
+# from subprocess import CalledProcessError
+
+
+# def build_inline_kb(*buttons: list[str]) -> InlineKeyboardMarkup:
+
+#     kb_builder = InlineKeyboardBuilder()
+#     kb_builder.row(*buttons)
+#     kb_builder.adjust(3)
+#     return kb_builder.as_markup()
