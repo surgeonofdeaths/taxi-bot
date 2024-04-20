8522f86f4dad4d7d5facff22eaf1479b2e6667eb
Artem Holodyr <artemmholodyr@gmail.com>
2024-04-21 01:07:21 +0300

    keyboard implementation

--- bot/keyboards/factory_kb.py
+++ bot/keyboards/factory_kb.py
@@ -1,8 +1,5 @@
-# from aiogram.filters.callback_data import CallbackData
+from aiogram.filters.callback_data import CallbackData
 
 
-# class OrderCallbackFactory(CallbackData, prefix="ord"):
-#     title: str | None
-#     uuid: str | None
-#     is_file: bool = True
-#     action: str  # dir, file
+class TestCallbackFactory(CallbackData, prefix="test"):
+    name: str | None
--- bot/keyboards/keyboard.py
+++ bot/keyboards/keyboard.py
@@ -17,3 +17,39 @@
 #     kb_builder.row(*buttons)
 #     kb_builder.adjust(3)
 #     return kb_builder.as_markup()
+
+# from keyboards.factory_kb import NavigationCallbackFactory
+
+from aiogram.utils.keyboard import (
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+    InlineKeyboardBuilder,
+    KeyboardButton,
+    KeyboardBuilder,
+    ReplyKeyboardMarkup,
+)
+
+# from keyboards.factory_kb import TestCallbackFactory
+
+
+def build_inline_kb(*buttons: list[str], adjust: int = 2) -> InlineKeyboardBuilder:
+    kb_builder = InlineKeyboardBuilder()
+    kb_builder.row(*buttons)
+    kb_builder.adjust(adjust)
+    return kb_builder
+
+
+def build_kb(*buttons: list[str]) -> KeyboardBuilder:
+    print("build_kb")
+    kb_builder = KeyboardBuilder(button_type=KeyboardButton)
+    kb_builder.add(*buttons)
+    return kb_builder
+
+
+def get_keyboard_markup() -> ReplyKeyboardMarkup:
+    button_contact = KeyboardButton(
+        text="Отправить свой контакт ☎️",
+        request_contact=True,
+    )
+    kb_markup = build_inline_kb(button_contact, adjust=2)
+    return kb_markup.as_markup()
