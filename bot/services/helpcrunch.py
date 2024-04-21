1abd467a74794021136bff0214ee9428a6c011f6
Artem Holodyr <artemmholodyr@gmail.com>
2024-04-21 03:21:49 +0300

    successful creation of a message from python to helpcrunch

--- bot/services/helpcrunch.py
+++ bot/services/helpcrunch.py
@@ -1,11 +1,11 @@
 import requests
 from loguru import logger
 import os
+from pprint import pprint
 
 URL = "https://api.helpcrunch.com/v1"
 BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
-
-headers = {
+HEADERS = {
     "Authorization": f"Bearer {BEARER_TOKEN}",
     "Content-Type": "application/json",
 }
@@ -15,13 +15,13 @@ def build_url(*parts):
     return "/".join(s.strip("/") for s in parts)
 
 
-def request_by_method(url, headers, method="GET"):
+def _request_by_method(url, headers, method: str = "get", json: dict | None = None):
     response = None
 
     if method.lower() == "get":
         response = requests.get(url, headers=headers)
     elif method.lower() == "post":
-        response = requests.post(url, headers=headers)
+        response = requests.post(url, json=json, headers=headers)
     elif method.lower() == "patch":
         response = requests.patch(url, headers=headers, data=None)
     elif method.lower() == "put":
@@ -31,24 +31,40 @@ def request_by_method(url, headers, method="GET"):
     return response
 
 
-def request_url(url, headers, method="get"):
+def _request_url(url, headers, method: str = "get", json: dict | None = None) -> dict:
     try:
-        response = request_by_method(url, headers, method)
+        response = _request_by_method(url, headers, method, json)
 
         if response.status_code == 200:
             logger.success("Request successful!")
-            return response.json()
         else:
             logger.error(f"Request failed with status code: {response.status_code}")
-            return response.text
-
+        return response.json()
     except Exception as e:
-        logger.info(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
 
 
-def get_all_messages(chat_id):
+def get_message(chat_id: int, message_id: int | None = None) -> dict:
     url = build_url(URL, "chats", str(chat_id), "messages")
-    print(request_url(url, headers, method="get"))
+
+    request = _request_url(url, HEADERS, method="get")
+    if not request.get("errors") and message_id:
+        # TODO: get corresponding message by message_id
+        pass
+    return request
 
 
-get_all_messages(1)
+def create_message(json: dict | None = None):
+    url = build_url(URL, "messages")
+    request = _request_url(url, HEADERS, method="post", json=json)
+    return request
+
+
+def get_customers():
+    pass
+
+
+if __name__ == "__main__":
+    # pprint(get_message(1))
+    json = {"chat": 1, "text": "A message from python code", "type": "message"}
+    print(create_message(json))
