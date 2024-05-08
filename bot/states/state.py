822647d002c004cc5e1530986045a8e3640b38a5
Artem Holodyr <artemmholodyr@gmail.com>
2024-05-08 17:46:08 +0300

    StartData state to minimize db queries

--- bot/states/state.py
+++ bot/states/state.py
@@ -1,13 +1,17 @@
 from aiogram.fsm.state import StatesGroup, State
 
 
+class StartData(StatesGroup):
+    start = State()
+
+
 class Order(StatesGroup):
     getting_phone = State()
     writing_start_address = State()
     writing_destination_address = State()
     writing_note = State()
     confirmation = State()
-    cancel_or_leave = State()
+    cancel_or_keep = State()
 
 
 class Conversation(StatesGroup):
