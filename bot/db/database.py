166f65bfbef594b9a6489808cac5dc2acf5d0163
Artem Holodyr <artemmholodyr@gmail.com>
2024-05-12 07:23:30 +0300

    remove verbosity of sqlalchemy

--- bot/db/database.py
+++ bot/db/database.py
@@ -6,5 +6,5 @@ from loguru import logger
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
 logger.info(f"DB url: {settings.db.url}")
-engine = create_async_engine(url=settings.db.url, echo=True)
+engine = create_async_engine(url=settings.db.url, echo=False)
 sessionmaker = async_sessionmaker(engine, expire_on_commit=False)
