f9736ccc9feb1fc707403e1f999ad7578d41042a
Artem Holodyr <artemmholodyr@gmail.com>
2024-06-10 02:03:44 +0300

    railway: add redis url

--- bot/misc.py
+++ bot/misc.py
@@ -4,4 +4,6 @@ from redis.asyncio import Redis
 # redis = Redis(host="localhost", port=6379, db=0)
 
 # prod
-redis = Redis(host="localhost", port=6379, db=0)
+# redis = Redis(host="localhost", port=6379, db=0)
+url = "redis://default:mZFFMApsmrwCSNtIBcqZyIzOWLHoLydl@monorail.proxy.rlwy.net:37402"
+redis = Redis.from_url(url=url)
